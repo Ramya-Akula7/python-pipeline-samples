45c0c22f98a35abda9f2606003b7bac1112584d5
Ramya-Akula7 <ramya.akula1618@gmail.com>
2026-02-19 14:06:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ app =   Flask(__name__)
 def hello_world():
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True , host='0.0.0.0')
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
